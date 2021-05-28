aba73b07d554549300ea7d48e478f1093f5f644e
Dominik Wagner <dominik.r.mall2@gmail.com>
2021-05-28 14:35:50 +0200

    moved selections to side bar, added map

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 import datetime
 import requests
 
@@ -6,15 +7,25 @@ import requests
 # Taxi Fare Calculator
 '''
 
-date = st.date_input("Pickup Date", datetime.date(2021,5,28))
-time = st.time_input("Pickup Time", datetime.time(13,0))
-date_time = str(date) + " " + str(time)
-pickup_lon = st.number_input("Pickup Longitude",)
-pickup_lat = st.number_input("Pickup Latitude",)
-dropoff_lon = st.number_input("Dropoff Longitude",)
-dropoff_lat = st.number_input("Dropoff Latitude",)
-passenger_count = st.slider("Number of passengers",1,5)
+pickup_lon = -73.9798156
+pickup_lat = 40.7614327
+dropoff_lon = -73.8803331
+dropoff_lat = 40.6513111
 
+date = st.sidebar.date_input("Pickup Date", datetime.date(2021,5,28))
+time = st.sidebar.time_input("Pickup Time", datetime.time(13,0))
+date_time = str(date) + " " + str(time)
+pickup_lon = st.sidebar.number_input("Pickup Longitude", value=pickup_lon)
+pickup_lat = st.sidebar.number_input("Pickup Latitude", value=pickup_lat)
+dropoff_lon = st.sidebar.number_input("Dropoff Longitude", value=dropoff_lon)
+dropoff_lat = st.sidebar.number_input("Dropoff Latitude", value=dropoff_lat)
+passenger_count = st.sidebar.slider("Number of passengers",1,5)
+
+map_loc = pd.DataFrame({'lon': [pickup_lon, dropoff_lon], 'lat': [pickup_lat, dropoff_lat]},index=['pickup', 'dropoff'])
+
+map_loc
+
+st.map(data=map_loc,zoom=10)
 
 url = 'https://taxifare.lewagon.ai/predict'
 
