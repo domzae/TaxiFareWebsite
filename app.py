0ec65ce22838cc68e72b3dc3fc15c5747d1f441b
Dominik Wagner <dominik.r.mall2@gmail.com>
2021-11-18 15:43:56 +0100

    .

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import datetime
 import requests
+import pydeck as pdk
 
 '''
 # Taxi Fare Calculator
@@ -38,5 +39,40 @@ if st.button('Calculate fare!'):
     prediction = response.json()['prediction']
     #st.markdown("## Expected fare:")
     st.write("${0:.2f}".format(prediction))
+
+
+
+###
+# Testing pdk
+###
+
+#view_state = pdk.ViewState(latitude=40.7614327, longitude=-73.9798156, zoom=10, bearing=0, pitch=0)
+# view_state = pdk.ViewState(latitude=37.7749295, longitude=-122.4194155, zoom=11, bearing=0, pitch=45)
+
+# TRIPS_LAYER_DATA = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf.trips.json"  # noqa
+# df = pd.read_json(TRIPS_LAYER_DATA)
+
+# df["coordinates"] = df["waypoints"].apply(lambda f: [item["coordinates"] for item in f])
+# df["timestamps"] = df["waypoints"].apply(lambda f: [item["timestamp"] - 1554772579000 for item in f])
+# df["coordinates"] = [[-122.39079879999997, 37.7664413], [-122.3908298, 37.9664413]]
+# df.drop(["waypoints"], axis=1, inplace=True)
+# st.pydeck_chart(pdk.Deck(
+#     map_style='mapbox://styles/mapbox/streets-v11',
+#     initial_view_state=view_state,
+#     layers = [pdk.Layer(
+#         "TripsLayer",
+#         df,
+#         get_path="coordinates",
+#         get_timestamps="timestamps",
+#         get_color=[253, 128, 93],
+#         opacity=0.8,
+#         width_min_pixels=5,
+#         rounded=True,
+#         trail_length=600,
+#         current_time=500,)]
+# ))
+###
+# End testing pdk
+###
 
 st.map(data=map_loc,zoom=10)
